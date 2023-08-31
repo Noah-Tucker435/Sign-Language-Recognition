6f766ef34c68ce591d8d5ccab6d5fed7d46bb65d
Noah Tucker <Ngtucker1234@gmail.com>
2023-08-31 15:11:27 -0500

    Create train_gesture.py
    Starting work for training the model on the dataset created in the gesture file.

---
+++ train_gesture.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D, Dropout
+from keras.optimizers import Adam, SGD
+from keras.metrics import categorical_crossentropy
+from keras.preprocessing.image import ImageDataGenerator
+import itertools
+import random
+import warnings
+import numpy as np
+import cv2
+from keras.callbacks import ReduceLROnPlateau
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
+train_path = r'C:\\GestureData\\Train\\'
+test_path = r'C:\\GestureData\\Test\\'
